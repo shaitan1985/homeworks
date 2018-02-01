c1b2aa2d5384b1fbc2341231a99a3a4e97c98cd1
Kirill Shaytanov <atray@mail.ru>
2018-02-01 23:28:13 +0300

    changed to 31/12

--- task_03_01.py
+++ task_03_01.py
@@ -2,7 +2,7 @@ from datetime import datetime, date
 
 def get_days_to_new_year():
     today = datetime.today()
-    source_date = date(today.year+1, 1,1)
+    source_date = date(today.year, 12,31)
     return source_date.toordinal() - today.toordinal()
 
 if __name__ == '__main__':
