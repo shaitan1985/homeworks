c9161cbc9652bfcb3d5b423595542b8f6b944530
shaitan1985 <atray@mail.ru>
2018-02-05 15:24:54 +0300

    union calc and deleted log

--- calc.py
+++ calc.py
@@ -8,6 +8,51 @@ from operator import (
 
 
 OPERATORS = {'^': pow, '*':mul,'/': truediv, '+': add, '-': sub}
+PRT = {'^': 3, '*': 2,'/': 2, '+': 1, '-': 1, '(': 0 }
+
+
+def same_or_lower(i, top):
+    return True if PRT[i] <= PRT[top] else False
+
+
+def move_stack(i, stack, outlst):
+    if not stack or i == '(':
+        stack.append(i)
+    else:
+        if i == ')':
+            temp = stack.pop()
+            while temp != '(':
+                outlst.append(temp + ' ')
+                temp = stack.pop()
+
+        elif same_or_lower(i, stack[len(stack) - 1]) :
+            temp_i = stack.pop()
+            if same_or_lower(i, stack[len(stack) - 1]):
+                outlst.append(temp_i + ' ')
+                temp_i = stack.pop()
+            stack.append(i)
+
+            return temp_i + ' '
+        else:
+            stack.append(i)
+
+    return ''
+
+
+def convert(expr):
+    templst = ''.join(expr.split())
+    outlst = []
+    stack = []
+    for i in templst:
+        if i not in "*/^+-()":
+            outlst.append(i + ' ')
+        else:
+            outlst.append(move_stack(i, stack, outlst))
+
+    while stack:
+        outlst.append(stack.pop() + ' ')
+
+    return ''.join(outlst).strip()
 
 
 def calc(expr):
@@ -22,7 +67,3 @@ def calc(expr):
         else:
             stack.append(float(i))
     return opn
-
-
-if __name__ == '__main__':
-    print(calc(input()))
--- task_http_headers.py
+++ task_http_headers.py
@@ -1,8 +1,5 @@
 import json
 
-from testlog import logging_debug as save_bd
-
-
 def find_prtcl(f_str):
     out_dict = {}
     lst = f_str.split()
@@ -24,14 +21,12 @@ def parse_str(t_str):
     ind = t_str.find(':')
     key = t_str[:ind]
     value = t_str[ind+2:].strip()
-    save_bd(key, value)
     return key, value
 
 
 def get_source_lst(tfile):
     with open(tfile) as f:
         lst = f.readlines()
-    save_bd(lst)
     return lst
 
 def save_to_file(tfile, tmp_dict):
@@ -44,21 +39,16 @@ def http_headers_to_json(source_file, output_file):
 
     first_line = lst[0]
     lst.remove(first_line)
-    save_bd(first_line)
-    save_bd(lst)
 
     out_dict = find_prtcl(first_line)
     for i in lst:
         tmp_key, tmp_vle = parse_str(i)
         out_dict[tmp_key] = tmp_vle
-    save_bd(out_dict)
     save_to_file(output_file, out_dict)
-
 
 
 
 if __name__ == '__main__':
 
     for i in range(1,4):
-        save_bd('*' * 60)
         http_headers_to_json('headers-{}.txt'.format(str(i)), 'results-{}.json'.format(str(i)))
