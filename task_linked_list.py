b1c72d2d604ca2dbd9b365c371f2c53f4d6de096
Kirill Shaytanov <atray@mail.ru>
2018-03-21 15:31:40 +0300

    debug smth in lst

--- task_linked_list.py
+++ task_linked_list.py
@@ -32,8 +32,9 @@ class LinkedList(object):
     def get_prev_by_value(self, value):
         item = self.head
         while item.link is not None:
-            if item.data == value:
+            if item.link.data == value:
                 return item
+            item = item.link
         return None
 
 
@@ -50,12 +51,17 @@ class LinkedList(object):
 
 
     def insert(self, index, value):
-        if index > self.indices:
+        if index > self.indices or self.head is None:
             self.add(value)
             return
-        item = self.get_by_index(index-1)
-        new_item = ListItem(value, item.link)
-        item.link = new_item
+        if index == 0:
+            item = self.head
+            self.head = ListItem(value, item)
+        else:
+            item = self.get_by_index(index-1)
+            new_item = ListItem(value, item.link)
+            item.link = new_item
+        self.indices += 1
 
 
     def get(self, index):
@@ -65,21 +71,36 @@ class LinkedList(object):
 
 
     def remove(self, value):
-        prev = get_prev_by_value(value)
-        if prev is None:
-            raise ValueError
-        prev.link = prev.link.link
+
+        if self.head is None:
+            return
+        if self.head.data == value:
+            if self.head.link is not None:
+                self.head = self.head.link
+            else:
+                self.clear()
+        else:
+            prev = self.get_prev_by_value(value)
+            if prev is None:
+                raise ValueError
+            prev.link = prev.link.link
+        self.indices -= 1
 
 
     def remove_at(self, index):
         if index == 0:
-            value = self.head.data
-            self.head = self.head.link
+            if self.head.link is not None:
+                value = self.head.data
+                self.head = self.head.link
+            else:
+                value = self.head.data
+                self.head = None
         else:
-            prev = get_by_index(index - 1)
+            prev = self.get_by_index(index - 1)
             value = prev.link.data
             prev.link = prev.link.link
 
+        self.indices -= 1
         return value
 
 
@@ -88,10 +109,12 @@ class LinkedList(object):
 
 
     def contains(self, value):
-        return get_prev_by_value(value) is not None
-
+        return self.get_prev_by_value(value) is not None
 
     def len(self):
+        return self.__len__()
+
+    def __len__(self):
         if self.head is None:
             return 0
         return self.indices + 1
@@ -102,13 +125,27 @@ class LinkedList(object):
 
 
     def __iter__(self):
-        self.copy = self.indices
+        self.copy = self.indices + 1
         return self
 
     def __next__(self):
-        while self.copy:
+        while self.copy :
             self.copy -= 1
             return self.get_by_index(self.indices - self.copy).data
         raise StopIteration
 
+
+if __name__ == '__main__':
+    import random
+    ll2 = LinkedList(6, -8, 7, 0, 6, 3, -5, -3, 5, 0, -2, 9, -10)
+    print([l for l in ll2])
+    ll2.add(20)
+    print([l for l in ll2])
+    print(ll2.len())
+    ll2.insert(0, 30)
+    for i in range(ll2.len()):
+        item = random.randrange(ll2.len())
+        print(item)
+        print(ll2.remove_at(item))
+        print([l for l in ll2])
 
